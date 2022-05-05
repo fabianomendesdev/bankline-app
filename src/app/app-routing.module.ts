2fb0e07006e659ecf07340cd30eb51706b8a75b1
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-05 12:30:05 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
+  { path: 'movimentacoes', component: MovimentacaoListComponent },
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.css']
+})
+export class MovimentacaoListComponent implements OnInit {
+  movimentacoes:any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
