42489c441be126792f80ae8d50b77949d1c78d79
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-07 13:16:13 -0300

    final update

--- src/app/components/correntista/correntista.component.ts
+++ src/app/components/correntista/correntista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
 
 @Component({
   selector: 'app-correntista',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./correntista.component.css']
 })
 export class CorrentistaComponent implements OnInit {
+  cpf:any;
+  nome:any;
+  correntistas:any;
 
-  constructor() { }
+  constructor(private correntistaService: CorrentistaService) { }
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
   }
 
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  save(): void {
+    const correntista = {
+      cpf:this.cpf,
+      nome:this.nome
+    };
+    console.log(correntista);
+    this.correntistaService.create(correntista)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.exibirCorrentistas();
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -13,4 +13,7 @@ export class CorrentistaService {
   list(): Observable<any> {
     return this.http.get(`${baseUrl}/correntistas`);
   }
+  create(correntista:any): Observable<any> {
+    return this.http.post(`${baseUrl}/correntistas`,correntista);
+  }
 }
